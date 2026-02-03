5a1b6396ec7765fee79983d85f9b1d164c34dcc4
maxim_kio <enderbroxa@yandex.ru>
2026-02-03 17:13:21 +0300

    handbook navbar local

--- src/javascript/components/O_PartList.jsx
+++ src/javascript/components/O_PartList.jsx
@@ -4,15 +4,8 @@ import C_CollapsibleLists from './C_CollapsibleLists.jsx'
 function O_PartList({ part }) {
   return (
     <div className="O_PartList">
-      {/*
-        Заголовок части — статичный текст
-        Используем класс subtitle, как в твоём примере HTML
-      */}
       <p className="subtitle">{part.title}</p>
 
-      {/*
-        Список глав этой части
-      */}
       <C_CollapsibleLists chapters={part.chapters} />
     </div>
   )
