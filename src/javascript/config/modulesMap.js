437204008f695080e23c345a65e7b81fe31bee4b
maximkio <enderbroxa@yandex.ru>
2026-05-23 21:39:30 +0300

    bunch of new things

--- src/javascript/config/modulesMap.js
+++ src/javascript/config/modulesMap.js
@@ -2,6 +2,8 @@ import movementIcon from '../../images/modules/moduleMovement-40.svg'
 import rotationIcon from '../../images/modules/moduleRotaion-40.svg'
 import transformIcon from '../../images/modules/moduleTransform-40.svg'
 import textIcon from '../../images/modules/moduleText-40.svg'
+import clickIcon from '../../images/modules/moduleClick-40.svg'
+import dragIcon from '../../images/modules/moduleDrag-40.svg'
 
 const modulesMap = {
   Движение: {
@@ -23,6 +25,16 @@ const modulesMap = {
     url: '/modules/static/text.html',
     language: 'CSS',
     icon: textIcon
+  },
+  Клик: {
+    url: '/modules/dynamic/click.html',
+    language: 'JavaScript',
+    icon: clickIcon
+  },
+  Перетаскивание: {
+    url: '/modules/dynamic/drag.html',
+    language: 'JavaScript',
+    icon: dragIcon
   }
 }
 
