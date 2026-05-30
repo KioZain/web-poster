b05d727dbd7a2fe91ee1e6a7638db5344fa210d2
maximkio <enderbroxa@yandex.ru>
2026-05-30 14:24:13 +0300

    W_PostersGrid bug fixed

--- src/javascript/components/C_PostersGrid.jsx
+++ src/javascript/components/C_PostersGrid.jsx
@@ -3,30 +3,28 @@ import PosterCard from './A_PosterCard.jsx'
 import notFound from '../../images/notfound.svg'
 
 function PostersGrid({ posters, isLoading, hasMore, onLoadMore }) {
-  if (posters.length === 0) {
-    return (
-      <div>
+  return (
+    <div className="W_PostersWrap">
+      {posters.length === 0 ? (
         <div className="M_NoContent">
           <img src={notFound} alt="" />
           <h4>Ничего не найдено</h4>
           <p className="light">Такой работы не существует</p>
         </div>
-      </div>
-    )
-  }
+      ) : (
+        <>
+          <div className="C_PostersGrid">
+            {posters.map((poster) => (
+              <PosterCard key={poster.id} poster={poster} />
+            ))}
+          </div>
 
-  return (
-    <div className="W_PostersWrap">
-      <div className="C_PostersGrid">
-        {posters.map((poster) => (
-          <PosterCard key={poster.id} poster={poster} />
-        ))}
-      </div>
-
-      {hasMore && (
-        <button onClick={onLoadMore} className="A_Button secondary">
-          Показать ещё
-        </button>
+          {hasMore && (
+            <button onClick={onLoadMore} className="A_Button secondary">
+              Показать ещё
+            </button>
+          )}
+        </>
       )}
     </div>
   )
